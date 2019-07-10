3275da292cf10e78b84153c90f08b264ab36e5d2
nan2411 <ncruztelle@gmail.com>
2019-07-10 16:25:39 -0500

    cuestionario admin edit

--- src/components/cuestionario/teacher.js
+++ src/components/cuestionario/teacher.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class CuestionarioTeacher extends Component {
     constructor(props) {
         super(props);
         /*Borra pregunta*/
-        this.handleClickDelete =id =>{
+        this.handleClickDelete =id => {
             if (this.state.questions.length > 1){
                 var question=[];
                 this.state.questions.map((item, i) =>{
@@ -12,13 +13,18 @@ class CuestionarioTeacher extends Component {
                         question.push(item);
                     }
                 });
+
                 this.state.questions = question;
+                console.log(question)
+                // var a = document.getElementById("divOpciones-"+id.currentTarget.id);
+                // console.log('a  '+ a)
+                // a.remove();
                 this.createDivs();
                 this.forceUpdate();
             }
         };
         /*Borra opcion*/
-        this.handleClickDeleteOption =id =>{
+        this.handleClickDeleteOption =id => {
             var ids=id.currentTarget.id.split('-');
             if (ids[1] > 1){
                 var options=[];
@@ -30,20 +36,34 @@ class CuestionarioTeacher extends Component {
                             }
                         });
                         this.state.questions[i].options=options;
+                        console.log(this.state.questions[i].options)
                     }
                 });
-                this.createDivs();
-                this.forceUpdate();
+                // console.log(this.state)
             }
+            this.createDivs();
+            this.forceUpdate();
         };
         /*Pinta opciones*/
-        this.createOptions = values =>{
+        this.createOptions = values => {
              var options = values.map((value, i) =>{
                 // console.log(value);
 
             });
             return <div>hola</div>;
 
+        }
+        /*Change en pregunta*/
+        this.handleChangeEventQuestion = value => {
+            var index =value.currentTarget.id.split('-')[1];
+            this.state.questions[index].question= value.currentTarget.value;
+        }
+        /**Change opciones*/
+        this.handleChangeEventOption = value => {
+            var i =value.currentTarget.id.split('-')[1];
+            var j =value.currentTarget.id.split('-')[2];
+            this.state.questions[i].options[j]= value.currentTarget.value;
+            console.log(this.state.questions[i].options)
         }
         /*this. state*/
         this.state={
@@ -55,7 +75,9 @@ class CuestionarioTeacher extends Component {
                         'Misisipi',
                         'Amazonas',
                         'Nilo',
-                        'Ninguno de los anteriores'
+                        'Amazonasssss',
+                        'oooo',
+                        'Ninguno',
                     ],
                 }
             ],
@@ -63,11 +85,12 @@ class CuestionarioTeacher extends Component {
         this.createDivs();
     }
 
-
     createDivs(){
         var click = this.handleClickDelete;
         var opciones = this.createOptions;
         var deleteOption = this.handleClickDeleteOption;
+        var handleChangeEventQuestion = this.handleChangeEventQuestion;
+        var handleChangeEventOption = this.handleChangeEventOption;
         this.state.divs = this.state.questions.map(
             function iterator (value, i){
                 var style = i >0 ? {display:'block'} : {display:'none'};
@@ -76,7 +99,13 @@ class CuestionarioTeacher extends Component {
                        var styleOption = j > 1 ? {display:'block'} : {display:'none'};
                        return(
                             <div className="option">
-                                <input value={option} type="text" className="question-option" />
+                                <input
+                                    defaultValue={option}
+                                    type="text"
+                                    id={'option-'+i+'-'+j}
+                                    className="question-option"
+                                    onChange={handleChangeEventOption}
+                                />
                                 <button className="div-delete-option" id={i+'-'+j} style={styleOption} onClick={deleteOption}></button>
                             </div>
                        );
@@ -86,13 +115,19 @@ class CuestionarioTeacher extends Component {
                     <div className="question-admin">
                         <div className="div-question">
                             <div>Pregunta {i+1}</div>
-                            <input value={value.question} type="text" className="question-text-admin" />
+                            <input
+                                defaultValue={value.question}
+                                onChange={handleChangeEventQuestion}
+                                type="text"
+                                className="question-text-admin"
+                                id={'question-'+i}
+                            />
                             <div className="type-question">
                                 <div><input type="radio"  value="3" name={i} className="radio" checked/>Respuesta abierta</div>
                                 <div><input type="radio"  value="2" name={i} className="radio" />Opcion multiple</div>
                                 <div><input type="radio"  value="1" name={i} className="radio" />Selección</div>
                             </div>
-                            <div className="questions-options">
+                            <div className="questions-options" id={"divOpciones-"+i}>
                                 {options}
                             </div>
                         </div>
